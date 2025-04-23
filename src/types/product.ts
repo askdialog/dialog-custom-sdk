be4cf1b40b28757f469847b7584e069ec4516054
Benjamin Changala <benjaminchangala@benjamins-mbp.home>
2025-04-23 15:00:34 +0200

    fix: product interface

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,8 +1,45 @@
+export interface SimplifiedProductVariant {
+  id: string;
+  title?: string;
+  displayName?: string;
+  selectedOptions?: {
+    name: string;
+    value: string;
+  }[];
+  image?: {
+    url?: string;
+  } | null;
+  inventoryQuantity?: number;
+  price?: string;
+  currencyCode?: string;
+  compareAtPrice?: string | null;
+  translations?: {
+    key: string;
+    value: string;
+  }[];
+}
+
+export interface SimplifiedProductOption {
+  translations?: {
+    key: string;
+    value: string;
+  }[];
+  values: string[];
+  id: string;
+  name: string;
+  position: number;
+}
+
 export interface SimplifiedProduct {
   id: string;
   variantId?: string;
   title: string;
-  shortDescription: string;
-  formattedPrice: string;
-  imageUrl: string;
+  descriptionHtml?: string;
+  featuredImage?: {
+    url?: string;
+  } | null;
+  totalInventory: number;
+  variants: SimplifiedProductVariant[];
+  options?: SimplifiedProductOption[];
+  handle: string;
 }
